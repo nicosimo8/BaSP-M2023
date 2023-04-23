150e695f3504c6629385bf9c2d1764efdbc4f531
Nicolas Simonetti <racingclub_nico@hotmail.com>
2023-04-23 15:25:23 -0300

    BaSP-W06-2023: minor changes for sign-up style, all validations added to sign-up.js

--- Week-06/js/sign-up.js
+++ Week-06/js/sign-up.js
@@ -10,17 +10,6 @@ window.onload = function() {
   var emailInput = document.querySelector('input[name="email"]');
   var passwordInput = document.querySelector('input[name="password"]');
   var passwordRepeatInput = document.querySelector('input[name="password-repeat"]');
-  var nameLabel = document.querySelector('label[for="first-name"]');
-  var lastNameLabel = document.querySelector('label[for="last-name"]');
-  var dniLabel = document.querySelector('label[for="dni"]');
-  var dateBirthLabel = document.querySelector('label[for="date-birth"]');
-  var phoneLabel = document.querySelector('label[for="phone"]');
-  var adressLabel = document.querySelector('label[for="adress"]');
-  var cityLabel = document.querySelector('label[for="city"]');
-  var postalCodelLabel = document.querySelector('label[for="postal-code"]');
-  var emailLabel = document.querySelector('label[for="email"]');
-  var passwordLabel = document.querySelector('label[for="password"]');
-  var passwordRepeatLabel = document.querySelector('label[for="password-repeat"]');
   var errorWarning = document.getElementsByClassName('sign-up-form-error');
   var form = document.getElementById('sign-up-form');
 
@@ -29,7 +18,7 @@ window.onload = function() {
   var formInputElements = document.querySelectorAll('form input');
 
   //oninput methods
-  function dateWriting(e){ ///////////////REVIEW!!!!!!///////////////
+  /*function dateWriting(e){ ///////////////REVIEW!!!!!!///////////////
     var input = e;
     var template = 'xx/xx/xxxx';
     var n = 0;
@@ -68,7 +57,7 @@ window.onload = function() {
 
   formInputElements[3].oninput = function() {
     formInputElements[3].value = dateWriting(formInputElements[3].value);
-  };
+  };*/
 
   //onBlur Methods
   function errorCheckDisplay() {
@@ -197,53 +186,157 @@ window.onload = function() {
     errorCheckRemove();
   };
 
-  function nameValidationError(){
-
-  };
-
   //inputs validation
-  function errorLabelAdd(errorString, labelIndex){
+  function errorLabelAdd(errorString, labelIndex) {
     formInputElements[0].classList.add('warningAsterisk');
     var errorLabel = document.createElement('p');
-    errorLabel.className = 'sign-up-form-field-error';
+    errorLabel.className = 'sign-up-form-field-error added';
     errorLabel.appendChild(document.createTextNode(errorString));
     formFieldElements[labelIndex].appendChild(errorLabel);
+  };
+
+  function errorLabelRemove(){
+    var formParagElements = document.querySelectorAll('form p');
+
+    for(i = 0; i < (formParagElements.length); i++){
+      if(formParagElements[i].classList.contains('added')){
+        var element = formFieldElements[i - 1];
+        while(element.children.length > 2) {
+          element.removeChild(element.children[2]);
+        }
+      };
+    };
   }
 
-  function inputValidationCheck(e){
+  function stringRunner(string) {
+    var strMatch = 0;
+    var numMatch = 0;
+    var speMatch = 0;
+    var spaces = 0;
+
+    for(i = 0; i < string.length; i++) {
+      if(string[i].search(/[a-z]/i) != -1) {
+        strMatch ++;
+      }else{
+        if(string[i].search(/[0-9]/) != -1) {
+          numMatch++;
+        }else {
+          speMatch ++;
+          if(string[i].search(" ") != -1) {
+            spaces++;
+          };
+        };
+      };
+    };
+
+    var coincidences = [strMatch, numMatch, speMatch, spaces];
+
+    return coincidences;
+  };
+
+  function inputValidationCheck(e) {
     e.preventDefault();
+
     //name field
-    if(formInputElements[0].value.length < 3){
+    if(formInputElements[0].value.length < 3) {
       errorLabelAdd('Must contain at least 3 characters', 0);
     };
+
     //last name field
-    if(formInputElements[1].value.length < 3 || formInputElements[1].value.length > 12){
+    if(formInputElements[1].value.length < 3 || formInputElements[1].value.length > 12) {
       errorLabelAdd('Must contain at least 3 and less than 12 characters', 1);
     };
+
     //dni field
-    if(formInputElements[2].value.length <= 7 || formInputElements[2].value.length > 10){
+    if(formInputElements[2].value.length <= 7 || formInputElements[2].value.length > 10) {
       errorLabelAdd('Must contain at least 3 and less than 10 characters', 2);
     };
+
     //date of birth field
-    if(typeof formInputElements[4] != 'number'){
-      errorLabelAdd('Only number allowed', 4);
+    var birth = formInputElements[3].value;
+    if(stringRunner(birth)[0] > 0 || stringRunner(birth)[2] > 0) {
+      errorLabelAdd('Only numbers allowed', 3);
     };
+
     //phone field
-    if(typeof formInputElements[4] != 'number'){
-      errorLabelAdd('Only number allowed', 4);
+    var phone = formInputElements[4].value;
+    if(stringRunner(phone)[0] > 0 || stringRunner(phone)[2] > 0) {
+      errorLabelAdd('Only numbers allowed', 4);
     };
-  };
 
-  function emailValidationCheck(e){
-    e.preventDefault();
+    //adress field
+    var adress = formInputElements[5].value;
+    var position = adress.search(" ");
+
+    if(stringRunner(adress)[0] + stringRunner(adress)[1] < 5) {
+      errorLabelAdd('Must contain at least 5 characters', 5);
+    };
+
+    if(stringRunner(adress)[0] < 3 || stringRunner(adress)[1] < 2) {
+      errorLabelAdd('Street name must contain at least 3 characters and number at least 2 characters', 5);
+    };
+
+    if(stringRunner(adress)[2] < 1) {
+      if(stringRunner(adress)[3] < 1 || stringRunner(adress)[3] > 1) {
+        errorLabelAdd('Must contain 1 space', 5);
+      };
+    };
+
+    if(stringRunner(adress)[2] > 1) {
+      if(stringRunner(adress[position - 1])[0] == 0 && stringRunner(adress[position + 1])[1] == 0){
+        errorLabelAdd('Spaces must be between the street name and the number', 5);
+      };
+    };
+    
+    //city field
+    var city = formInputElements[6].value;
+
+    if(city.length < 3) {
+      errorLabelAdd('Must contain at least 3 characters', 6);
+    };
+
+    if(stringRunner(city)[2] > 1 && stringRunner(city)[3] == 0) {
+      errorLabelAdd('Only spaces allowed', 6);
+    };
+
+    //postal code field
+    var postalCode = formInputElements[7].value;
+
+    if(stringRunner(postalCode)[0] > 0 || stringRunner(postalCode)[2] > 0) {
+      errorLabelAdd('Only numbers allowed', 7);
+    };
+
+    if(stringRunner(postalCode)[1] < 4 || stringRunner(postalCode)[1] > 5) {
+      errorLabelAdd('Must contain 4 or 5 digits', 7);
+    };
+
+    //email field
     var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
-    var emailValue = emailInput.value;
-    if(emailExpression.test(emailValue) != true){
+    var email = formInputElements[8].value;
 
-    }else{
-      alert('Log-In success!');
+    if(emailExpression.test(email) != true) {
+      errorLabelAdd('Must contain a valid format', 8);
     };
+
+    //password field
+    var pass = formInputElements[9].value;
+
+    if(pass.length < 8 || pass.length > 12) {
+      errorLabelAdd('Must contain between 8 and 12 characters', 10);
+    };
+
+    if(stringRunner(pass)[2] < 1){
+      errorLabelAdd('Must contain at least 1 special character', 10);
+    }
+
+    //password repeat field
+    var passRep = formInputElements[10].value;
+
+    if(passRep != pass){
+      errorLabelAdd('Passwords did not match', 11);
+    }
   };
 
+  form.addEventListener('submit', errorLabelRemove);
   form.addEventListener('submit', inputValidationCheck);
 };
